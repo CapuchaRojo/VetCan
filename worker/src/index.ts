8d75d6574329fcb0dae0a44f872adb264e02c509
A Storm Is Coming LLC <adam.hoodtech@gmail.com>
2025-12-07 17:34:53 -0500

    Initialize workers for outbound calls and emails

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,4 +1,19 @@
-import "./queues/outboundCallQueue";
-import "./queues/emailQueue";
+import { Worker } from 'bullmq';
+import IORedis from 'ioredis';
 
-console.log("Worker online");
+
+const connection = new IORedis(process.env.REDIS_URL);
+
+
+const callWorker = new Worker('outbound-calls', async job => {
+console.log('Processing outbound call job', job.id, job.data);
+// call Twilio adapter via API or SDK
+}, { connection });
+
+
+const emailWorker = new Worker('email-send', async job => {
+console.log('Processing email job', job.id, job.data);
+}, { connection });
+
+
+console.log('Worker started');

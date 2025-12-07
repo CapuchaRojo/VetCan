999748e0cea24b182cfc98e00634b76b762bf88f
A Storm Is Coming LLC <adam.hoodtech@gmail.com>
2025-12-07 17:30:28 -0500

    Refactor TwilioService to TwilioAdapter with webhook handler

--- api/src/services/TwilioAdapter.ts
+++ api/src/services/TwilioAdapter.ts
@@ -1,25 +1,25 @@
-import { TelephonyAdapter } from "./TelephonyAdapter";
-import twilio from "twilio";
+import { TelephonyAdapter } from './TelephonyAdapter';
+import twilio from 'twilio';
 
-export class TwilioService implements TelephonyAdapter {
-  client = twilio(
-    process.env.TWILIO_ACCOUNT_SID,
-    process.env.TWILIO_AUTH_TOKEN
-  );
 
-  async dial(number: string) {
-    return this.client.calls.create({
-      to: number,
-      from: process.env.TWILIO_NUMBER,
-      url: "https://example.com/twilio/outbound"
-    });
-  }
-
-  async hangup(callId: string) {
-    return this.client.calls(callId).update({ status: "completed" });
-  }
-
-  async createInboundWebhook(req: any) {
-    return { ok: true, message: "Inbound webhook received" };
-  }
+export class TwilioAdapter implements TelephonyAdapter {
+client: any;
+constructor() {
+this.client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+}
+async dial(phone: string) {
+return this.client.calls.create({
+to: phone,
+from: process.env.TWILIO_NUMBER,
+url: `${process.env.TWILIO_WEBHOOK_BASE}/twilio/outbound`
+});
+}
+async hangup(callSid: string) {
+return this.client.calls(callSid).update({ status: 'completed' });
+}
+async webhookHandler(req: any, res: any) {
+// Minimal handler: acknowledge
+res.type('text/xml');
+res.send('<Response></Response>');
+}
 }
